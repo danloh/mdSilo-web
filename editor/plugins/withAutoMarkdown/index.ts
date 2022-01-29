8ed25e8a2a2dfcbeccf643e4e34d9c641b613c59
danloh <lploh2016@gmail.com>
2022-01-28 19:29:49 -0800

    fix:  Pasting in code block, show seperately

--- editor/plugins/withAutoMarkdown/index.ts
+++ editor/plugins/withAutoMarkdown/index.ts
@@ -10,6 +10,7 @@ const withAutoMarkdown = (editor: Editor, isWiki: boolean) => {
   editor.insertText = (text) => {
     const handled = handleAutoMarkdown(editor, text, isWiki);
     if (!handled) {
+      // text = text.replace(/\r\n?/g, '\n');
       insertText(text);
     }
   };
@@ -17,6 +18,14 @@ const withAutoMarkdown = (editor: Editor, isWiki: boolean) => {
   editor.insertData = (data) => {
     const text = data.getData('text/plain');
     const handled = handleAutoMarkdown(editor, text, isWiki);
+
+    // in code block
+    if (handled === undefined) {
+      const txt = text.replace(/\r\n?/g, '\n');
+      insertText(txt);
+      return;
+    }
+    
     if (!handled) {
       insertData(data);
     }
@@ -35,7 +44,7 @@ const handleAutoMarkdown = (editor: Editor, text: string, isWiki: boolean) => {
   });
 
   if (inCodeBlock) {
-    return;
+    return undefined;
   }
 
   // Handle shortcuts at the beginning of a line
