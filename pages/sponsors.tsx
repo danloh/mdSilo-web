59cecd90cb630b0e0c643d6b5e7d6cc1ffcf999e
danloh <lploh2016@gmail.com>
2022-08-15 14:41:15 -0700

    chore: add a donate link

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import MainView from 'components/landing/MainView';
+import payqr from 'public/stripe-qr.png';
 
 export default function Sponsors() {
   return (
@@ -11,8 +13,19 @@ export default function Sponsors() {
           mdSilo is an <a href="https://github.com/danloh/mdSilo-web" className="link" target="_blank" rel="noopener noreferrer">open source project</a> and is provided for free
         </p>
         <p className="text-xl text-center mt-4">
-          We appreciate any support from you: recommend it to your friends, report bugs, contribute in coding...
+          We appreciate any support from you: 
         </p>
+        <p className="text-xl text-center mt-4">
+          recommend it to your friends, report bugs, contribute in coding or donate...
+        </p>
+        <div className="container p-6 flex flex-col items-center justify-center">
+          <Image
+            src={payqr}
+            width={220}
+            height={263.75} 
+          />
+          <a href="https://buy.stripe.com/6oE9ChgVZeOT4WA000" className="link mt-2 text-xl" target="_blank" rel="noopener noreferrer">Click to donate</a>
+        </div>
       </div>
     </MainView>
   );
