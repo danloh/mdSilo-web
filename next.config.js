82b165c44c3ddd2017571f7e0c3ae76d65c1780b
danloh <lploh2016@gmail.com>
2022-10-22 15:37:14 -0700

    fix: refresh

--- next.config.js
+++ next.config.js
@@ -1,52 +1,46 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const withPWA = require('next-pwa');
-
 const { withSentryConfig } = require('@sentry/nextjs');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  scope: '/app/',
+  disable: process.env.NODE_ENV === 'development',
+  dynamicStartUrlRedirect: '/app/',
+  reloadOnOnline: false,
+  register: false,
+  skipWaiting: false,
+});
 
+const isSentryEnabled = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
+  // see: https://github.com/getsentry/sentry-webpack-plugin#options.
   silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
-  return withPWA(
-    withSentryConfig(
-      withBundleAnalyzer({
-        experimental: { esmExternals: true },
-        trailingSlash: true,
-        pwa: {
-          dest: 'public',
-          scope: '/',
-          disable: process.env.NODE_ENV === 'development',
-          dynamicStartUrlRedirect: '/',
-          reloadOnOnline: false,
-          register: false,
-          skipWaiting: false,
-        },
-        i18n: {
-          locales: ['en-US'],
-          defaultLocale: 'en-US',
-        },
-        env: {
-          BASE_URL: isDev ? 'http://localhost:3000' : 'https://mdsilo.com',
-        },
-      }),
-      SentryWebpackPluginOptions
-    )
+  let config = withPWA(
+    withBundleAnalyzer({
+      experimental: { esmExternals: true },
+      trailingSlash: true,
+      i18n: {
+        locales: ['en-US'],
+        defaultLocale: 'en-US',
+      },
+      env: {
+        BASE_URL: isDev ? 'http://localhost:3000' : 'https://mdsilo.com',
+      },
+    })
   );
+
+  if (isSentryEnabled) {
+    config = withSentryConfig(config, SentryWebpackPluginOptions);
+  }
+
+  return config;
 };
